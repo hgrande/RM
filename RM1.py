e6a3dfdc383a74e43e0834c8d8bf1c6a184ec4c9
hgrande <52891798+hgrande@users.noreply.github.com>
2022-06-01 16:58:00 +0200

    Add files via upload

--- RM1.py
+++ RM1.py
@@ -1,22 +1,49 @@
-import sys
+a0=[-0.3, -0.5, -1.0, -2.0, -3.0, -3.3, -3.5]  # elasticities in linear demand model
+b0=300  # constants in the linear demand model 
+sigma=10. # the standard deviation of the noise
+price_levels=[5, 8, 10, 12, 13, 16, 19] # predefined possible prices for each day
+probabilities=[0.3, 0.3, 0.2, 0.05, 0.05, 0.05, 0.05]  # the probabilities of taking a price choice at a day
+n_samples=1000  # the number of sample we want to create
 
-import numpy as np
-import pandas as pd
+def create_data_point(p, a, b, sigma):
+    """
+    estimate the demand
+    :param p: np.array, (T,)
+    :param a: np.array, (T,)
+    :param b: float, the constants
+    :return: v
+    """
+    v = np.dot(p,a) + b + np.random.normal(loc=0.0, scale=sigma)
+    return v
 
-sys.path.append('../')
-from revpy.revpy import protection_levels
-from revpy.helpers import cumulative_booking_limits
-from revpy.fare_transformation import calc_fare_transformation
+def create_dataset(a, b, N, price_levels, probabilities, sigma):
+    """
+    create a dataset for training the demand model
+    :param a: np.array, (T,)
+    :param b: float
+    :param N: int number of samples
+    :param price_levels: list, price levels
+    :param probabilities: list, probabilities distribution of the prices
+    :return:
+    """
+    t = len(a)
+    prices = np.random.choice(price_levels, N+t-1, p=probabilities)
+    data_x = []
+    data_y = []
+    for i in range(N):
+        p = prices[i:i+t]
+        v = create_data_point(p, a, b, sigma)
+        data_x.append(
+            np.expand_dims(p, axis=0)
+        )
+        data_y.append(v)
 
-fares = np.array([1200, 1000, 800, 600, 400, 200])
-demands = np.array([31.2, 10.9, 14.8, 19.9, 26.9, 36.3])
-sigmas = np.array([11.2, 6.6, 7.7, 8.9, 10.4, 12])
-capacity = 100
-classes = np.arange(len(fares)) + 1
-class_names = ["class_{}".format(c) for c in classes]
+    data_x = np.concatenate(data_x,axis=0)
+    return data_x, data_y
 
-adjusted_fares, adjusted_demand, Q, TR = calc_fare_transformation(fares, demands, return_all=True)
+data_x, data_y = create_dataset(a0, b0, n_samples, price_levels, probabilities, sigma)
 
-data = np.transpose(np.vstack((fares, demands, Q, TR, adjusted_fares)))
-pd.DataFrame(data, index=class_names, columns=['fares', 'demand', 'Q', 
-                                               'TR', 'MR/adjusted fares'])
+print("dataset x first 10 samples:")
+print(data_x[:10])
+print("dataset y first 10 samples:")
+print(data_y[:10])
